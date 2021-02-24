dbb0a394c45fbbfec9a42db1acec3181d43a2622
Janrupf <business.janrupf@gmail.com>
2021-02-24 19:52:18 +0100

    Fade between dioramas

--- lib/components/multi_animated_cross_fade.dart
+++ lib/components/multi_animated_cross_fade.dart
@@ -1,10 +1,53 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class MultiAnimatedCrossFade extends StatefulWidget {
+  final List<Widget> children;
+  final Duration changeDuration;
+  final Duration displayDuration;
+
+  MultiAnimatedCrossFade(
+      {Key? key,
+      required this.children,
+      required this.changeDuration,
+      required this.displayDuration})
+      : super(key: key);
+
   @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    throw UnimplementedError();
+  State<StatefulWidget> createState() => _MultiAnimatedCrossFadeState();
+}
+
+class _MultiAnimatedCrossFadeState extends State<MultiAnimatedCrossFade> {
+  late final Timer _displayTimer;
+
+  int _index = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _displayTimer = Timer.periodic(widget.displayDuration, (timer) => _swap());
   }
 
+  @override
+  void dispose() {
+    _displayTimer.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) => AnimatedSwitcher(
+        duration: widget.changeDuration,
+        child: widget.children[_index],
+      );
+
+  void _swap() {
+    if (!mounted) {
+      return;
+    }
+
+    setState(() {
+      _index = (_index + 1) % widget.children.length;
+    });
+  }
 }
--- lib/views/waiting_view.dart
+++ lib/views/waiting_view.dart
@@ -1,30 +1,41 @@
+import 'package:eep_bridge_host/components/multi_animated_cross_fade.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 class WaitingView extends StatelessWidget {
   @override
   Widget build(BuildContext context) => Scaffold(
-        body: Container(
-          decoration: BoxDecoration(
-              image: DecorationImage(
-                  image: AssetImage("assets/dioramas/01.png"),
-                  fit: BoxFit.cover)),
-          alignment: Alignment.bottomCenter,
-          padding: EdgeInsets.only(bottom: 20),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text(
-                Intl.message("Waiting for clients..."),
-                style: TextStyle(fontSize: 40),
+        body: Stack(
+          fit: StackFit.expand,
+          children: [
+            FittedBox(
+                child: MultiAnimatedCrossFade(
+                  changeDuration: Duration(seconds: 1),
+                  displayDuration: Duration(seconds: 30),
+                  children: [
+                    Image.asset("assets/dioramas/01.png", key: ValueKey(1),),
+                    Image.asset("assets/dioramas/02.png", key: ValueKey(2))
+                  ],
+                ),
+                fit: BoxFit.cover),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    Intl.message("Waiting for clients..."),
+                    style: TextStyle(fontSize: 40),
+                  ),
+                  Text(
+                    Intl.message("Load up a layout which uses EEPBridge!"),
+                    style: TextStyle(fontSize: 20),
+                  )
+                ],
               ),
-              Text(
-                Intl.message("Load up a layout which uses EEPBridge!"),
-                style: TextStyle(fontSize: 20),
-              )
-            ],
-          ),
+            ),
+          ],
         ),
       );
 }
