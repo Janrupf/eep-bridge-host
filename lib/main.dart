9302c6d9ee335737b260fdcbd03638d27deb60b5
Janrupf <business.janrupf@gmail.com>
2021-08-05 20:38:30 +0200

    Save projects on close (linux)

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:eep_bridge_host/components/auto_unfocus.dart';
+import 'package:eep_bridge_host/logging/logger.dart';
 import 'package:eep_bridge_host/network/bridge_server.dart';
 import 'package:eep_bridge_host/project/controller.dart';
 import 'package:eep_bridge_host/util/application_theme.dart';
 import 'package:eep_bridge_host/views/viewport_wrapper.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 
 void main() {
@@ -28,26 +30,43 @@ void main() {
 }
 
 /// Main application widget, wraps the theme consumer.
-class EEPBridgeHost extends StatelessWidget {
+class EEPBridgeHost extends StatefulWidget {
+  @override
+  State<EEPBridgeHost> createState() => _EEPBridgeHostState();
+}
+
+class _EEPBridgeHostState extends State<EEPBridgeHost> {
+  final windowCloseChannel = MethodChannel("eep_bridge/window_events");
+
+  @override
+  void initState() {
+    super.initState();
+    windowCloseChannel.setMethodCallHandler(_onWindowEvent);
+  }
+
   @override
   Widget build(BuildContext context) => Consumer<ApplicationTheme>(
       builder: (context, theme, child) => AutoUnfocus(
-            child: WillPopScope(
-              onWillPop: () => _onPopScope(context),
-              child: MaterialApp(
-                debugShowCheckedModeBanner: false,
-                initialRoute: "/",
-                routes: {"/": (context) => ViewportWrapper()},
-                title: "EEPBridgeHost",
-                theme: ApplicationTheme.light(),
-                darkTheme: ApplicationTheme.dark(),
-                themeMode: theme.currentTheme(),
-              ),
+            child: MaterialApp(
+              debugShowCheckedModeBanner: false,
+              initialRoute: "/",
+              routes: {"/": (context) => ViewportWrapper()},
+              title: "EEPBridgeHost",
+              theme: ApplicationTheme.light(),
+              darkTheme: ApplicationTheme.dark(),
+              themeMode: theme.currentTheme(),
             ),
           ));
 
-  Future<bool> _onPopScope(BuildContext context) async {
-    await Provider.of<ProjectController>(context, listen: false).saveAll();
-    return true;
+  Future<void> _onWindowEvent(MethodCall call) async {
+    if(call.method == "Window.close") {
+      try {
+        Logger.debug("Saving projects...");
+        await Provider.of<ProjectController>(context, listen: false).saveAll();
+        Logger.info("Window closed, saved projects, exiting...");
+      } catch(e, stackTrace) {
+        Logger.error("Failed to save projects", e, stackTrace);
+      }
+    }
   }
 }
